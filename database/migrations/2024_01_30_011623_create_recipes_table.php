12ed551f1955eb7e68a3e5a16372870f404b7ea9
Ken Chapple <ken.chapple@gmail.com>
2024-01-31 22:53:12 -0500

    Adds front end components, routes and backend support

--- database/migrations/2024_01_30_011623_create_recipes_table.php
+++ database/migrations/2024_01_30_011623_create_recipes_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description');
             $table->string('author_email');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
